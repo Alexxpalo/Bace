d368d7949a71b733379c737a8881d606681646b5
Alexxpalo <104994634+Alexxpalo@users.noreply.github.com>
2022-05-07 17:49:35 +0300

    v 0.0.1
    game is now published, next project is a  better scoreboard system.

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 var foodspawner = Math.floor(Math.random() * 5000);
-var x = 0; var y = 0; var i = 0;
+var x = 0; var y = 0; var i = 0; var socketid;
 console.log(foodspawner);
 
 function spawn_food() {
     i = i + 1;
     x = Math.floor(Math.random() * 1400);
-    y = Math.floor(Math.random() * 750);
-    console.log(x, y)
+    y = Math.floor(Math.random() * 700);
     io.emit("Spawn_food_f", x, y);
 } 
 
@@ -20,17 +19,17 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on("connection", (socket) => {
-    console.log(socket.id);
-    spawn_food();
 
     socket.on("Food_Clicked", (score, food_x, food_y) => {
-        console.log(socket.id, score, food_x, food_y);
+        socketid = socket.id;
+        io.emit("Delete_Food", socketid, score, food_x, food_y);
     });
 
 });
 
-
+setInterval(spawn_food, 1500);
 
 
 server.listen(3000, () => {
